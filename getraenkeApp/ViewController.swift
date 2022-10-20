36cb8192cf332760989676d4229889ba965a43c6
jacquelineschmitz <jacquelineschmitz81@gmail.com>
2022-10-20 10:42:10 +0200

    Initial Commit

---
+++ getraenkeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getraenkeApp
+//
+//  Created by Jacqueline Schmitz on 20.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
